9eb10b126b32b62752d0fe2a7e227989e4e1564a
Bekzodjon Norkuziev <bekzodnx@gmail.com>
2022-08-05 21:45:40 +0200

    Navbar fix

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,54 +1,54 @@
 import { Outlet, Link, NavLink } from 'react-router-dom';
 
-let activeClassName = 'underline underline-offset-4';
+let activeClassName = 'border-b-yellow-600 border-b-4 py-3';
 
 function Navbar() {
   return (
     <>
-      <nav className='bg-bluerose text-white py-4 px-5 bold flex overflow-x-scroll space-x-5 text-xl'>
+      <nav className='px-4 bg-bluerose text-white bold flex overflow-x-scroll space-x-5 text-xl '>
         <NavLink
           to='/state'
-          className={({ isActive }) => (isActive ? activeClassName : '')}
+          className={({ isActive }) => (isActive ? activeClassName : 'py-3')}
         >
           useState
         </NavLink>
         <NavLink
           to='/effect'
-          className={({ isActive }) => (isActive ? activeClassName : '')}
+          className={({ isActive }) => (isActive ? activeClassName : 'py-3')}
         >
           useEffect
         </NavLink>
         <NavLink
           to='/context'
-          className={({ isActive }) => (isActive ? activeClassName : '')}
+          className={({ isActive }) => (isActive ? activeClassName : 'py-3')}
         >
           useContext
         </NavLink>
         <NavLink
           to='/ref'
-          className={({ isActive }) => (isActive ? activeClassName : '')}
+          className={({ isActive }) => (isActive ? activeClassName : 'py-3')}
         >
           useRef
         </NavLink>
         <NavLink
           to='/reducer'
-          className={({ isActive }) => (isActive ? activeClassName : '')}
+          className={({ isActive }) => (isActive ? activeClassName : 'py-3')}
         >
           useReducer
         </NavLink>
         <NavLink
           to='/memo'
-          className={({ isActive }) => (isActive ? activeClassName : '')}
+          className={({ isActive }) => (isActive ? activeClassName : 'py-3')}
         >
           useMemo
         </NavLink>
         <NavLink
           to='/callback'
-          className={({ isActive }) => (isActive ? activeClassName : '')}
+          className={({ isActive }) => (isActive ? activeClassName : 'py-3')}
         >
           useCallback
         </NavLink>
-        <button className=''>useLayoutEffect</button>
+        <button className='border-b-yellow-600'>useLayoutEffect</button>
       </nav>
       <Outlet />
     </>
